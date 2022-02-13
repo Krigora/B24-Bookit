36edf4f6945b05796bd1c586d1c0388142ccc834
krigora <ihor.kryvynskyy@icloud.com>
2022-02-13 16:19:31 -0600

    Create new project (ui + api) add manu practice. After working need VERY GOOD read framework (spac: util.Environment for read different query. Add differnt quory: RUN => Edit Configurations => + => choose MAVEN => command file written our one query or copy configuration. After than we have run each separate query

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,20 +1,86 @@
 package com.bookit.step_definitions;
 
+import com.bookit.pages.SelfPage;
 import com.bookit.utilities.BookItApiUtil;
-import com.bookit.utilities.DBUtils;
 import com.bookit.utilities.Environment;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.Assert;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import com.bookit.utilities.BookItApiUtil;
+import com.bookit.utilities.Environment;
+import io.restassured.http.ContentType;
 
 import java.util.Map;
 
-import static io.restassured.RestAssured.*;
-
 public class ApiStepDefs {
 
+    String accessToken;
+    Response response;
+
+    @Given("User logged in to Bookit api as teacher role")
+    public void user_logged_in_to_Bookit_api_as_teacher_role() {
+        accessToken = BookItApiUtil.getAccessToken(Environment.TEACHER_EMAIL, Environment.TEACHER_PASSWORD);
+        System.out.println("Teacher email = " + Environment.TEACHER_EMAIL);
+        System.out.println("Teacher password = " + Environment.TEACHER_PASSWORD);
+
+    }
+
+
+    @Given("User sends GET request to {string}")
+    public void user_sends_GET_request_to(String path) {
+        response = given().accept(ContentType.JSON)
+                .and().header("Authorization", accessToken)
+                .when().get(Environment.BASE_URL + path);
+        System.out.println("API Endpoint = " + Environment.BASE_URL + path);
+        response.prettyPrint();
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(Integer expStatusCode) {
+        assertThat(response.statusCode(), equalTo(expStatusCode));
+
+    }
+
+    @Then("content type is {string}")
+    public void content_type_is(String expContentType) {
+        assertThat(response.contentType(), equalTo(expContentType));
+
+    }
+
+
+    @And("role is {string}")
+    public void roleIs(String expRole) {
+        JsonPath json = response.jsonPath();
+        System.out.println("role= " + json.getString("role"));
+        assertThat(json.getString("role"), is(expRole));
+    }
+
+    /**
+     *
+     */
+    @Then("User should see same info on UI and API")
+    public void user_should_see_same_info_on_UI_and_API() {
+
+        Map<String, Object> apiUserMap = response.body().as(Map.class);
+        String apiFullName = apiUserMap.get("firstName")+" " +apiUserMap.get("lastName");
+        System.out.println("apiFullName = " + apiFullName);
+
+        //read value from UI using POM
+        SelfPage selfPage = new SelfPage();
+        String uiFullName = selfPage.fullName.getText();
+        String uiRole = selfPage.role.getText();
+
+        System.out.println("uiFullName = " + uiFullName);
+
+        assertThat(uiFullName, equalTo(apiFullName));
+        assertThat(uiRole, equalTo(apiUserMap.get("role")));
+
+    }
 }
---
+++ src/test/java/com/bookit/step_definitions/UIStepDefs.java
@@ -0,0 +1,27 @@
+package com.bookit.step_definitions;
+
+import com.bookit.pages.HomePage;
+import com.bookit.pages.LoginPage;
+import com.bookit.utilities.Driver;
+import com.bookit.utilities.Environment;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class UIStepDefs {
+
+    @Given("User logged in to Bookit app as teacher role")
+    public void user_logged_in_to_Bookit_app_as_teacher_role() {
+        Driver.getDriver().get(Environment.URL);
+        LoginPage loginPage=new LoginPage();
+        loginPage.login(Environment.TEACHER_EMAIL, Environment.TEACHER_PASSWORD);
+    }
+
+    @Given("User is on self page")
+    public void user_is_on_self_page() {
+        HomePage homePage = new HomePage();
+        homePage.gotoSelf();
+
+
+
+    }
+}
